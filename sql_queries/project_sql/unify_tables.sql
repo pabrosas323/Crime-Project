737c598e484418f90cbaf1b1481eaa116b5ccbc7
Pablo Rosas <prosas1@lsu.edu>
2024-09-02 17:01:47 -0400

    quick proof of concept

--- sql_queries/project_sql/unify_tables.sql
+++ sql_queries/project_sql/unify_tables.sql
@@ -10,4 +10,46 @@ so that they can be more organized and the data more
 accessible. 
 
 */
+SELECT
+        nibrs_incident_2020.data_year,
+        nibrs_incident_2020.agency_id,
+        incident_id,
+        nibrs_month_id,
+        cargo_theft_flag,
+        submission_date,
+        incident_date,
+        report_date_flag,
+        incident_hour
+        cleared_except_id,
+        cleared_except_date,
+        data_home,
+        orig_format,
+        did
+FROM
+    nibrs_incident_2020
 
+LEFT JOIN agencies_2020
+    ON nibrs_incident_2020.agency_id = agencies_2020.agency_id
+UNION ALL
+SELECT
+        nibrs_incident_2021.data_year,
+        nibrs_incident_2021.agency_id,
+        incident_id,
+        nibrs_month_id,
+        cargo_theft_flag,
+        submission_date,
+        incident_date,
+        report_date_flag,
+        incident_hour
+        cleared_except_id,
+        cleared_except_date,
+        data_home,
+        orig_format,
+        did
+FROM
+    nibrs_incident_2021
+
+LEFT JOIN agencies_2021
+    ON nibrs_incident_2021.agency_id = agencies_2021.agency_id
+
+LIMIT 100;
